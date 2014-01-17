2afa95a378473da2d643fa9d341c0d05fd86b8b1
vyrak bunleang <vyrak.bunleang@gmail.com>
2014-01-16 21:35:12 -0700

    sass css importer

--- Gemfile
+++ Gemfile
@@ -5,7 +5,9 @@ ruby '1.9.3'
 gem 'puma'
 gem 'sinatra', :require => 'sinatra/base'
 gem 'slim'
+gem 'compass'
 gem 'sass'
+gem 'sass-css-importer'
 
 group :development do
   gem 'dotenv'
--- modules/app.rb
+++ modules/app.rb
@@ -6,8 +6,8 @@ class App < Sinatra::Base
   get '/css/*.css' do |file|
     content_type :css
     begin
-      scss :"styles/#{file}",
-        syntax: :scsss,
+      sass :"styles/#{file}",
+        syntax: :sass,
         line_numbers: true,
         style: :expanded
     rescue Errno::ENOENT
